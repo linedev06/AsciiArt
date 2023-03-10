8c388bee2e547f18c4cce726a198add4a1f7e513
linedev06 <vitorafelismino@gmail.com>
2023-03-10 20:53:35 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("AsciiArt");
+Console.WriteLine(" ||   ||");
+Console.WriteLine("  \\\\()//   ");
+Console.WriteLine(" //(__)\\\\");
+Console.WriteLine(" ||    ||");
